e69c7593887c19c40da84591da016bd5c82bf1f6
Jacob Patterson <jacob@jacobpa.com>
2019-09-25 19:44:56 -0400

    speaq api login test

--- src/utils/speaq-api.test.js
+++ src/utils/speaq-api.test.js
@@ -4,5 +4,22 @@ import axios from 'axios';
 jest.mock('axios');
 
 describe('speaq-api', () => {
+    describe('login', () => {
+        it('Should return true if the request is resolved without error', async () => {
+            axios.post.mockResolvedValue({});
+            const result = await login('username', 'password');
 
+            expect(result).toEqual(true);
+        });
+
+        it('Should return false if the request throws an error', async () => {
+            axios.post.mockImplementation(() => {throw new Error()})
+            console.error = jest.fn();
+
+            const result = await login('username', 'password');
+            
+            expect(console.error).toHaveBeenCalled();
+            expect(result).toEqual(false);
+        });
+    });
 });
