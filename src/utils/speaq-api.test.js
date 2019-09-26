6e33d6ae4cc57dadff97d2c4a327e0ea61bcbc69
Jacob Patterson <jacob@jacobpa.com>
2019-09-25 20:18:34 -0400

    check session tests

--- src/utils/speaq-api.test.js
+++ src/utils/speaq-api.test.js
@@ -24,12 +24,39 @@ describe('speaq-api', () => {
     });
 
     describe('logout', () => {
-        it('Should error if the request throws an error', async () => {
+        it('Should log error if the request throws an error', async () => {
             axios.post.mockImplementation(() => {throw new Error()})
             console.error = jest.fn();
 
-            const result = await logout();
+            await logout();
             
+            expect(console.error).toHaveBeenCalled();
+        });
+    });
+
+    describe('checkSession', () => {
+        it('Should return true if authenticated', async () => {
+            axios.get.mockResolvedValue({status: 200, data: '{"is_authenticated": true}'});
+
+            const result = await checkSession();
+
+            expect(result).toEqual(true);
+        });
+
+        it('Should return false if not authenticated', async () => {
+            axios.get.mockResolvedValue({status: 200, data: '{"is_authenticated": false}'});
+
+            const result = await checkSession();
+
+            expect(result).toEqual(false);
+        });
+
+        it('Should log error if request throws an error', async () => {
+            axios.get.mockImplementation(() => {throw new Error()});
+            console.error = jest.fn();
+
+            await checkSession();
+
             expect(console.error).toHaveBeenCalled();
         });
     });
